ce737deb3371e4c0938c16267bbd9f48e86e2ada
suhas30 <pesug20cs355@pesu.pes.edu>
2023-02-18 16:30:06 +0530

    Committed the Jenkins Lab main to the Github repository of PES2UG20CS355_Jenkins

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I (SUHAS T J, PES2UG20CS355) have successfully built and run this !\n";
+  return 0;
+}
